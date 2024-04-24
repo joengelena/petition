3c8ca966ee0982cce2f94494e18ca37fddcddcca
Elena Joeng <cjo108@uclive.ac.nz>
2024-04-24 13:42:21 +1200

    Added a new routes corresponding to the pages that has been created

--- assignment2/chat-app/src/App.tsx
+++ assignment2/chat-app/src/App.tsx
@@ -3,17 +3,24 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Petitions from "./components/Petitions";
 import NotFound from "./components/NotFound";
+import Register from "./components/Register";
+import LogIn from "./components/LogIn";
+
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
       <div className="App">
         <Router>
           <div>
+              <NavBar />
             <Routes>
-                <Route path="/petitions" element={<Petitions/>}/>
                 <Route path="" element={<Petitions/>}/>
-              <Route path="/petitions/:id" element={<Petitions/>}/>
-              <Route path="*" element={<NotFound/>}/>
+                <Route path="/petitions" element={<Petitions/>}/>
+                <Route path="/petitions/:id" element={<Petitions/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/logIn" element={<LogIn/>}/>
+                <Route path="*" element={<NotFound/>}/>
             </Routes>
           </div>
         </Router>
