8f5fd37cbe79bac8fffa2073feccf2e5b77555b2
Elena Joeng <cjo108@uclive.ac.nz>
2024-05-16 20:34:37 +1200

    The dialog that I can open when I need to tell login first before doing some action.

---
+++ assignment2/chat-app/src/components/LogInDialog.tsx
@@ -0,0 +1,42 @@
+import {Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
+import React from "react";
+import {useNavigate} from "react-router-dom";
+
+
+interface LogInDialogProps {
+    open: boolean;
+    onClose: () => void;
+}
+
+const LogInDialog: React.FC<LogInDialogProps> = (props) => {
+    const navigate = useNavigate();
+
+    const handleLogInDialogClose = () => {
+        props.onClose();
+    };
+
+
+    return (
+        <Dialog
+            open={props.open}
+            onClose={props.onClose}
+            aria-labelledby="login-modal-title"
+            aria-describedby="login-modal-description"
+        >
+            <DialogTitle id="login-dialog-title">Login Required</DialogTitle>
+            <DialogContent>
+                <DialogContentText id="logout-dialog-description">
+                    You need to log in to create a petition.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button style={{color: '#FF3333'}} onClick={handleLogInDialogClose}>Cancel</Button>
+                <Button onClick={()=>(navigate('/login'))} autoFocus>
+                    Log In
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default LogInDialog;
