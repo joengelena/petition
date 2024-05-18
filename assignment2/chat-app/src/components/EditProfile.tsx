231b4140248690f08b04f0fe0f0fb8f53ffbe279
Elena Joeng <cjo108@uclive.ac.nz>
2024-05-18 22:17:09 +1200

    Editing profile completed with seperate image uploading page.

--- assignment2/chat-app/src/components/EditProfile.tsx
+++ assignment2/chat-app/src/components/EditProfile.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import axios from "axios";
 import {useUserInfoStorage} from "../store";
-import {Alert, AlertTitle, Avatar, Box, Button, Paper, Stack, TextField, Typography} from "@mui/material";
-import {useNavigate} from "react-router-dom";
-import uploadImage from "./UploadImage";
+import {Alert, AlertTitle, Avatar, Button, Paper, Stack, TextField, Typography} from "@mui/material";
+import {Link, useNavigate} from "react-router-dom";
+import { CloudUpload } from "@mui/icons-material";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 const baseUrl = "http://localhost:4941/api/v1";
 
 
@@ -13,53 +14,111 @@ const EditProfile = () => {
     const [firstName, setFirstName] = React.useState("");
     const [lastName, setLastName] = React.useState("");
     const [email, setEmail] = React.useState("");
+    const [currentPassword, setCurrentPassword] = React.useState("");
+    const [newPassword, setNewPassword] = React.useState("");
+    const [currentPasswordVisible, setCurrentPasswordVisible] = React.useState(false);
+    const [newPasswordVisible, setNewPasswordVisible] = React.useState(false);
 
     const userLocal = useUserInfoStorage(state => state.user);
     const [errorFlag, setErrorFlag] = useState(false)
     const [errorMessage, setErrorMessage] = useState("")
     const [user, setUser] = React.useState<User>()
 
+    const [disableSave, setDisableSave] = React.useState(true)
+
 
     React.useEffect(() => {
-        const getUser = () => {
-            axios.get(`${baseUrl}/users/${userLocal.userId}`, {
-                headers: {
-                    "X-Authorization": userLocal.token
-                }
-            })
-                .then((response) => {
-                        setUser(response.data)
-                        setErrorFlag(false)
-                        setErrorMessage("")
-                    },
-                    (error) => {
-                        setErrorFlag(true)
-                        setErrorMessage(error.toString());
-                    })
-        }
         getUser()
-    })
+    }, [])
+
+    React.useEffect(()=>{
+        unDisableSave()
+    }, [firstName, lastName, email, newPassword])
+
+    const unDisableSave = () => {
+        if (firstName !== user?.firstName || lastName !== user?.lastName || email !== user?.email || newPassword !== "") {
+            setDisableSave(false)
+        } else {
+            setDisableSave(true)
+        }
+    }
+
+    const getUser = () => {
+        axios.get(`${baseUrl}/users/${userLocal.userId}`, {
+            headers: {
+                "X-Authorization": userLocal.token
+            }
+        })
+            .then((response) => {
+                const userData = response.data
+                setUser(userData)
+                setErrorFlag(false)
+                setErrorMessage("")
+                setFirstName(userData.firstName)
+                setLastName(userData.lastName)
+                setEmail(userData.email)
+            },
+            (error) => {
+                setErrorFlag(true)
+                setErrorMessage(error.status);
+            })
+    }
+    const updateUser = () => {
+        const data: EditUser = {};
+
+        if (firstName !== '') {
+            data.firstName = firstName
+        }
+        if (lastName !== '') {
+            data.lastName = lastName
+        }
+        if (email !== '') {
+            data.email = email
+        }
+        if (newPassword !== '') {
+            data.currentPassword = currentPassword;
+            data.password = newPassword;
+        }
+
+        axios.patch(`${baseUrl}/users/${userLocal.userId}`, data,{
+            headers: {
+                "X-Authorization": userLocal.token,
+                "Content-Type": "application/json"
+            }
+        })
+            .then((response) => {
+                navigate(`/profile`);
+                console.log(response)
+                setErrorFlag(false)
+                setErrorMessage("")
+            })
+            .catch((error) => {
+                setErrorFlag(true)
+                if (error.response.statusText.includes("email")) {
+                    setErrorMessage("Please enter a valid email address")
+                } else if (error.response.statusText.includes("6")) {
+                    setErrorMessage("Password must be at least 6 characters")
+                } else if (error.response.statusText === "Incorrect currentPassword") {
+                    setErrorMessage("Please enter a valid current password")
+                } else {
+                    setErrorMessage(error.response.statusText)
+                }
+                console.log(error.response)
+            })
+    }
 
     return (
         <div style={{padding: 50}}>
-            <Paper elevation={2} style={{padding: 20, margin: 'auto', maxWidth: 500}}>
-                <Typography variant="h3" style={{fontWeight: 'bold', padding: 10, marginBottom: "10px"}}>
+            <Paper elevation={2} style={{padding: 20, margin: 'auto', maxWidth: 450}}>
+                <Typography variant="h3" style={{fontWeight: 'bold'}}>
                     Edit Profile
                 </Typography>
                 <Stack direction="column" spacing={2} marginTop={2} marginBottom={2} justifyContent="center" alignItems="center">
                     <Avatar
-                        src={`${baseUrl}/users/${user?.userId}/image`}
+                        src={`${baseUrl}/users/${userLocal.userId}/image`}
                         alt={`${user?.lastName}`}
-                        style={{ width: 100, height: 100 }}
+                        style={{ width: 150, height: 150 }}
                     />
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        style={{ background:"#0f5132", marginTop: 8, marginBottom: 8 }}
-                        onClick={() => uploadImage()}
-                    >
-                        Update Image
-                    </Button>
                     <TextField
                         label="First Name"
                         variant="outlined"
@@ -86,8 +145,43 @@ const EditProfile = () => {
                         fullWidth
                         margin="normal"
                     />
+                    <Typography variant="h6" >Update Password</Typography>
+                    <TextField
+                        label="Current Password"
+                        variant="outlined"
+                        type={currentPasswordVisible ? "text" : "password"}
+                        value={currentPassword}
+                        placeholder="******"
+                        onChange={e => setCurrentPassword(e.target.value)}
+                        fullWidth
+                        InputProps={{
+                            endAdornment: (
+                                <Button onClick={() => setCurrentPasswordVisible(!currentPasswordVisible)}>
+                                    {currentPasswordVisible ? <VisibilityOff/> : <Visibility/>}
+                                </Button>
+                            )
+                        }}
+                        margin="normal"
+                    />
+                    <TextField
+                        label="Update Password"
+                        variant="outlined"
+                        type={newPasswordVisible ? "text" : "password"}
+                        value={newPassword}
+                        placeholder="******"
+                        onChange={e => setNewPassword(e.target.value)}
+                        fullWidth
+                        InputProps={{
+                            endAdornment: (
+                                <Button onClick={() => setNewPasswordVisible(!newPasswordVisible)}>
+                                    {newPasswordVisible ? <VisibilityOff/> : <Visibility/>}
+                                </Button>
+                            )
+                        }}
+                        margin="normal"
+                    />
                     {errorFlag &&
-                        <Alert severity="error">
+                        <Alert severity="error" style={{width: '100%'}}>
                             <AlertTitle>Error</AlertTitle>
                             {errorMessage}
                         </Alert>}
@@ -95,11 +189,14 @@ const EditProfile = () => {
                         variant="contained"
                         color="primary"
                         fullWidth
-                        onClick={()=> {navigate('/profile')}} // TODO also save the value
-                        style={{ marginBottom: 8 }}
+                        onClick={updateUser}
+                        disabled={disableSave}
                     >
                         Save
                     </Button>
+                    <Link to="/Profile" >
+                        Back
+                    </Link>
                 </Stack>
 
             </Paper>
